fc79e4fb991a16202296b6105f3b11fadc743185
Patrick Dowell <patrick.dowell@gmail.com>
2014-01-25 18:51:28 -0500

    added ejs support

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express =require('express');
 var app = express();
+var EJS = require('ejs');
 
 app.get('/hello.txt', function(req, res) {
   res.send('Hello World');
+});
+
+app.get('/test', function(req, res) {
+  res.render('template.ejs', {title: 'foo'});
 });
 
 app.listen(3000);
