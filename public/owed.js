8324a49091702d4d67cdf3a5c0059f1001ea2f2e
Patrick Dowell <patrick.dowell@gmail.com>
2014-05-26 16:49:26 -0400

    adding react.js

--- public/owed.js
+++ public/owed.js
@@ -1,6 +1,25 @@
+/**
+ * @jsx React.DOM
+ */
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
 $(document).ready(function(){
     $('.entries tr').click(function(){
         window.location = $(this).attr('href');
         return false;
     });
+
+    React.renderComponent(
+      <CommentBox />,
+      document.getElementById('content')
+    );
 });
+
